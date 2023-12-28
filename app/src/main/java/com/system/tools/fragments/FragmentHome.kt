9e3c38ee6236176d5e5d570477ba837ff321d198
libra12367 <145210465+libra12367@users.noreply.github.com>
2023-12-28 21:55:20 +0800

    Add files via upload

--- app/src/main/java/com/system/tools/fragments/FragmentHome.kt
+++ app/src/main/java/com/system/tools/fragments/FragmentHome.kt
@@ -169,54 +169,94 @@ class FragmentHome : androidx.fragment.app.Fragment() {
     @SuppressLint("SetTextI18n")
     private fun updateInfo() {
         if (coreCount < 1) {
-            coreCount = CpuFrequencyUtils.getCoreCount()
+            coreCount = CpuFrequencyUtil.getCoreCount()
+            myHandler.post {
+                try {
+                    cpu_core_count.text = "$coreCount 核心"
+                } catch (ex: Exception) {
+                }
+            }
         }
         val cores = ArrayList<CpuCoreInfo>()
-        val loads = CpuFrequencyUtils.getCpuLoad()
         for (coreIndex in 0 until coreCount) {
-            val core = CpuCoreInfo()
+            val core = CpuCoreInfo(coreIndex)
 
-            core.currentFreq = CpuFrequencyUtils.getCurrentFrequency("cpu$coreIndex")
-            if (!maxFreqs.containsKey(coreIndex) || (core.currentFreq != "" && maxFreqs.get(coreIndex).isNullOrEmpty())) {
-                maxFreqs.put(coreIndex, CpuFrequencyUtils.getCurrentMaxFrequency("cpu" + coreIndex))
+            core.currentFreq = CpuFrequencyUtil.getCurrentFrequency("cpu$coreIndex")
+            if (!maxFreqs.containsKey(coreIndex) || (core.currentFreq != "" && maxFreqs[coreIndex].isNullOrEmpty())) {
+                maxFreqs[coreIndex] = CpuFrequencyUtil.getCurrentMaxFrequency("cpu$coreIndex")
             }
-            core.maxFreq = maxFreqs.get(coreIndex)
+            core.maxFreq = maxFreqs[coreIndex]
 
-            if (!minFreqs.containsKey(coreIndex) || (core.currentFreq != "" && minFreqs.get(coreIndex).isNullOrEmpty())) {
-                minFreqs.put(coreIndex, CpuFrequencyUtils.getCurrentMinFrequency("cpu" + coreIndex))
+            if (!minFreqs.containsKey(coreIndex) || (core.currentFreq != "" && minFreqs[coreIndex].isNullOrEmpty())) {
+                minFreqs.put(coreIndex, CpuFrequencyUtil.getCurrentMinFrequency("cpu$coreIndex"))
             }
-            core.minFreq = minFreqs.get(coreIndex)
-
-            if (loads.containsKey(coreIndex)) {
-                core.loadRatio = loads.get(coreIndex)!!
-            }
+            core.minFreq = minFreqs[coreIndex]
             cores.add(core)
         }
-        val gpuFreq = GpuUtils.getGpuFreq() + "%"
+        val loads = cpuLoadUtils.cpuLoad
+        for (core in cores) {
+            if (loads.containsKey(core.coreIndex)) {
+                core.loadRatio = loads[core.coreIndex]!!
+            }
+        }
+
+        val gpuFreq = GpuUtils.getGpuFreq() + "Mhz"
         val gpuLoad = GpuUtils.getGpuLoad()
+
+        // 电池电流
+        batteryCurrentNow = batteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)
+        // 电量
+        val batteryCapacity = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        // 电池温度
+        val temperature = GlobalStatus.updateBatteryTemperature()
+
+        updateRamInfo()
+        val memInfo = memoryUtils.memoryInfo
+
         myHandler.post {
             try {
-                cpu_core_count.text = String.format(getString(R.string.monitor_core_count), coreCount)
+                home_swap_cached.text = "" + (memInfo.swapCached / 1024) + "MB"
+                home_buffers.text = "" + (memInfo.buffers / 1024) + "MB"
+                home_dirty.text = "" + (memInfo.dirty / 1024) + "MB"
+
+                home_running_time.text = elapsedRealtimeStr()
+                if (batteryCurrentNow != Long.MIN_VALUE && batteryCurrentNow != Long.MAX_VALUE) {
+                    home_battery_now.text = (batteryCurrentNow / globalSPF.getInt(SpfConfig.GLOBAL_SPF_CURRENT_NOW_UNIT, SpfConfig.GLOBAL_SPF_CURRENT_NOW_UNIT_DEFAULT)).toString() + "mA"
+                } else {
+                    home_battery_now.text = "--"
+                }
+                home_battery_capacity.text = "$batteryCapacity%"
+                home_battery_temperature.text = "${temperature}°C"
 
                 home_gpu_freq.text = gpuFreq
-                home_gpu_load.text = String.format(getString(R.string.monitor_laod1), gpuLoad)
+                home_gpu_load.text = "负载：$gpuLoad%"
                 if (gpuLoad > -1) {
                     home_gpu_chat.setData(100.toFloat(), (100 - gpuLoad).toFloat())
                 }
                 if (loads.containsKey(-1)) {
-                    cpu_core_total_load.text = String.format(getString(R.string.monitor_laod), loads.get(-1)!!.toInt())
-                    home_cpu_chat.setData(100.toFloat(), (100 - loads.get(-1)!!.toInt()).toFloat())
+                    cpu_core_total_load.text = "负载：" + loads[-1]!!.toInt().toString() + "%"
+                    home_cpu_chat.setData(100.toFloat(), (100 - loads[-1]!!.toInt()).toFloat())
                 }
                 if (cpu_core_list.adapter == null) {
+                    val layoutParams = cpu_core_list.layoutParams
                     if (cores.size < 6) {
+                        layoutParams.height = dp2px(105 * 2F)
                         cpu_core_list.numColumns = 2
+                    } else if (cores.size > 12) {
+                        layoutParams.height = dp2px(105 * 4F)
+                    } else if (cores.size > 8) {
+                        layoutParams.height = dp2px(105 * 3F)
+                    } else {
+                        layoutParams.height = dp2px(105 * 2F)
                     }
+                    cpu_core_list.layoutParams = layoutParams
                     cpu_core_list.adapter = AdapterCpuCores(context!!, cores)
                 } else {
                     (cpu_core_list.adapter as AdapterCpuCores).setData(cores)
                 }
+
             } catch (ex: Exception) {
-                Log.e("Exception", ex.message)
+
             }
         }
         updateTick++
@@ -224,6 +264,27 @@ class FragmentHome : androidx.fragment.app.Fragment() {
             updateTick = 0
             minFreqs.clear()
             maxFreqs.clear()
+        }
+    }
+
+    private fun setModeState() {
+        btn_powersave.alpha = 0.4f
+        btn_defaultmode.alpha = 0.4f
+        btn_gamemode.alpha = 0.4f
+        btn_fastmode.alpha = 0.4f
+        when (ModeSwitcher.getCurrentPowerMode()) {
+            ModeSwitcher.BALANCE -> {
+                btn_defaultmode.alpha = 1f
+            }
+            ModeSwitcher.PERFORMANCE -> {
+                btn_gamemode.alpha = 1f
+            }
+            ModeSwitcher.POWERSAVE -> {
+                btn_powersave.alpha = 1f
+            }
+            ModeSwitcher.FAST -> {
+                btn_fastmode.alpha = 1f
+            }
         }
     }
 
@@ -237,5 +298,68 @@ class FragmentHome : androidx.fragment.app.Fragment() {
     override fun onPause() {
         stopTimer()
         super.onPause()
+    }
+
+    private fun toggleMode(modeSwitcher: ModeSwitcher, mode: String): Deferred<Unit> {
+        return GlobalScope.async {
+            context?.run {
+                if (modeSwitcher.modeConfigCompleted()) {
+                    modeSwitcher.executePowercfgMode(mode, packageName)
+                } else {
+                    CpuConfigInstaller().installOfficialConfig(context!!)
+                    modeSwitcher.executePowercfgMode(mode, packageName)
+                }
+            }
+        }
+    }
+
+    private fun installConfig(toMode: String) {
+        val dynamic = AccessibleServiceHelper().serviceRunning(context!!) && spf.getBoolean(SpfConfig.GLOBAL_SPF_DYNAMIC_CONTROL, SpfConfig.GLOBAL_SPF_DYNAMIC_CONTROL_DEFAULT)
+        if (!dynamic && ModeSwitcher.getCurrentPowerMode() == toMode) {
+            modeSwitcher.setCurrent("", "")
+            globalSPF.edit().putString(SpfConfig.GLOBAL_SPF_POWERCFG, "").apply()
+            myHandler.post {
+                DialogHelper.confirmBlur(this.activity!!,
+                        "提示",
+                        "需要重启手机才能恢复默认调度，是否立即重启？",
+                        {
+                            KeepShellPublic.doCmdSync("sync\nsleep 1\nsvc power reboot || reboot")
+                        },
+                        null)
+                setModeState()
+            }
+            return
+        }
+
+        val progressBarDialog = ProgressBarDialog(this.activity!!, "home-mode-switch")
+        progressBarDialog.showDialog(getString(R.string.please_wait))
+
+        GlobalScope.launch(Dispatchers.Main) {
+            toggleMode(modeSwitcher, toMode).await()
+
+            setModeState()
+            maxFreqs.clear()
+            minFreqs.clear()
+            updateInfo()
+            if (dynamic) {
+                globalSPF.edit().putString(SpfConfig.GLOBAL_SPF_POWERCFG, "").apply()
+                DialogHelper.alert(activity!!,
+                        "提示",
+                        "“场景模式-动态响应”已被激活，你手动选择的模式随时可能被覆盖。\n\n如果你需要长期使用手动控制，请前往“功能”菜单-“性能界面”界面关闭“动态响应”！")
+            } else {
+                globalSPF.edit().putString(SpfConfig.GLOBAL_SPF_POWERCFG, toMode).apply()
+                if (!globalSPF.getBoolean(SpfConfig.GLOBAL_SPF_POWERCFG_FRIST_NOTIFY, false)) {
+                    DialogHelper.confirm(activity!!,
+                            "提示",
+                            "如果你已允许Scene自启动，手机重启后，Scene还会自动激活刚刚选择的模式。\n\n如果需要恢复系统默认调度，请再次点击，然后重启手机！",
+                            DialogHelper.DialogButton(getString(R.string.btn_confirm)),
+                            DialogHelper.DialogButton(getString(R.string.btn_dontshow), {
+                                globalSPF.edit().putBoolean(SpfConfig.GLOBAL_SPF_POWERCFG_FRIST_NOTIFY, true).apply()
+                            })).setCancelable(false)
+                }
+            }
+
+            progressBarDialog.hideDialog()
+        }
     }
 }
